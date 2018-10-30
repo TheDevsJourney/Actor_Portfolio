f7f672922b8c060b32a4733809ff0538029604c1
thedevsjourney <thedevsjourney@gmail.com>
2018-10-30 16:54:19 -0500

    First commit: Desktop version, needs to be made responsive

---
+++ js.js
@@ -0,0 +1,76 @@
+window.onload = function(){
+    var i = 0, text;
+    text = "Hey, I'm ";
+
+    function typing(){
+        if(i < text.length){
+            document.querySelector(".typedtext").innerHTML += text.charAt(i);
+            i++;
+            setTimeout(typing, 200);
+        }
+    }
+
+    var textrise = document.querySelector(".textrise");
+    textrise.setAttribute(
+        "style", "transform: translateY(0vh); opacity: 1;"
+    );
+
+    function timingSpan(){
+        var textspan = document.querySelectorAll(".textrise p span");
+        // Turning the nodelist of spans into an array and looping through each of them
+        Array.from(textspan).forEach(function(text){
+            // console.log(text.textContent)
+            text.setAttribute(
+                "style", "transform: translateX(0vw); opacity: 1;"
+            )
+        })
+    }
+    typing();
+    timingSpan();
+};
+
+function resume(){
+    var resume = document.querySelector(".resume");
+    // resume.setAttribute(
+    //     "style", "transform: translateX(0vw);"
+    // )
+    resume.classList.toggle("resumetog");
+}
+
+function exitResume(){
+    var resume = document.querySelector(".resume");
+    resume.classList.toggle("resumetog");
+}
+
+function toggleVideo(){
+    var displayvideo = document.querySelector(".displayvideo");
+    displayvideo.classList.toggle("revealvid");
+}
+
+
+window.onscroll = function(){
+    var scrollnav = document.querySelector(".scrollnav");
+    var header = document.querySelector(".headerintro");
+    var links = document.querySelectorAll(".scrollnav ul li a");
+    var uparrow = document.querySelector(".uparrow");
+
+    if(window.pageYOffset > header.scrollHeight){
+        scrollnav.classList.add("scrollnavshow");
+        for(var i = 0; i < links.length; i++){
+            links[i].setAttribute(
+                "style", "opacity: 1; transition: all 1s ease-in .3s; display: block"
+            )
+        }
+        uparrow.classList.add("uparrowshow");        
+    }else{
+        scrollnav.classList.remove("scrollnavshow");
+        for(var i = 0; i < links.length; i++){
+            links[i].setAttribute(
+                "style", "opacity: 0;"
+            )
+        }
+        uparrow.classList.remove("uparrowshow");
+    }
+}
+
+
